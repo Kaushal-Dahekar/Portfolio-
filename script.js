412c865ec27b0552f7d0f221fea82d48ee9fa905
Kaushal-Dahekar <138662238+Kaushal-Dahekar@users.noreply.github.com>
2023-09-12 11:57:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var typed= new Typed(".multiple-text ",{
+    strings:["Frontend Developer ","Web Developer " ,"App Developer "],
+    typeSpeed:100,
+    backSpeed:100,
+    typeDelay:1000,
+    loop:true
+    
+    
+})
